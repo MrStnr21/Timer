dc2ab6210328d469a2f2aadf8169b9a47f51c8c1
Yaroslav <MrStnr21@yandex.ru>
2022-11-17 14:01:11 +0300

    first commit

--- index.js
+++ index.js
@@ -1,45 +1,24 @@
-const inputEl = document.querySelector("input");
-const buttonEl = document.querySelector("button");
-const timerEl = document.querySelector("span");
-let timer;
+const inputEl = document.querySelector('input');
+const buttonEl = document.querySelector('button');
+const timerEl = document.querySelector('span');
 
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {
-    clearInterval(timer);
-    timer = setInterval(function () {
-      const sec = seconds % 60;
-      const min = (seconds / 60) % 60;
-      const hour = (seconds / 60 / 60) % 60;
-
-      if (seconds <= 0) {
-        clearInterval(timer);
-        timerEl.textContent = "Пришло время сделать мир чуточку лучше :)";
-      } else {
-        const startTimer = `${Math.trunc(hour)}:${Math.trunc(min)}:${sec}`;
-        timerEl.textContent = startTimer;
-      }
-      --seconds;
-    }, 1000);
-  };
+  return (seconds) => {};
 };
 
 const animateTimer = createTimerAnimator();
 
-inputEl.addEventListener("input", () => {
+inputEl.addEventListener('input', () => {
   // Очистите input так, чтобы в значении
   // оставались только числа
-  if (isNaN(inputEl.value)) {
-    inputEl.value = "";
-    alert("Пожалуйста,введите число.");
-  }
 });
 
-buttonEl.addEventListener("click", () => {
+buttonEl.addEventListener('click', () => {
   const seconds = Number(inputEl.value);
-  clearInterval(animateTimer);
+
   animateTimer(seconds);
 
-  inputEl.value = "";
+  inputEl.value = '';
 });
